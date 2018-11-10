6eed08314dbfc1ba21645e41ee7e575727112062
Ramakanta <33664106+rk472@users.noreply.github.com>
2018-11-10 13:31:19 +0530

    Done Adding Firebase

---
+++ app/src/main/java/com/smarttersstudio/crimeadmin/ComplainDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.smarttersstudio.crimeadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComplainDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complain_details);
+    }
+}
