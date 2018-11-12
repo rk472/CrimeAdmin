72c266bff43fc078d91509144c46b43be081b6d7
Ramakanta <33664106+rk472@users.noreply.github.com>
2018-11-12 23:13:10 +0530

    Done Adding Firebase

--- app/src/main/java/com/smarttersstudio/crimeadmin/MenuActivity.java
+++ app/src/main/java/com/smarttersstudio/crimeadmin/MenuActivity.java
@@ -1,6 +1,10 @@
 package com.smarttersstudio.crimeadmin;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,8 +15,20 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.CALL_PHONE},1);
+            return;
+        }
     }
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1)
+            if(grantResults[0]!=PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.CALL_PHONE},1);
+            }
+    }
     public void viewCrimes(View view) {
         startActivity(new Intent(this,CrimeActivity.class));
     }
